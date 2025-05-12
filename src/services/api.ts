eb371aac5ed7c636d553a9db30b21c4225f81677
AnkurRoy15 <104682247+AnkurRoy15@users.noreply.github.com>
2025-05-12 13:00:17 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const baseURL = 'http://192.168.29.94:5000'; // Replace with your Flask server IP
+const baseURL = 'http://#:5000'; // Replace with your Flask server IP
 
 const api = axios.create({
   baseURL,
